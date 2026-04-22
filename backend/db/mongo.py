1a36a1063f926edcd2c3fc4f6e4822cbfbefcdc2
Rei <reinosuke0801@gmail.com>
2026-04-22 22:40:11 +0900

    fix: mongoDBの接続先をatlasに変更

--- backend/db/mongo.py
+++ backend/db/mongo.py
@@ -1,6 +1,8 @@
 import os
+from dotenv import load_dotenv
 from pymongo import MongoClient
 
+load_dotenv()
 MONGO_URI = os.getenv("MONGO_URI")
 
 def get_collection(collection_name: str = "chunks"):
